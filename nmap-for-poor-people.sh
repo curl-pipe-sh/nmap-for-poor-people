c95f87bee73da7dc0828d2de5376d18a66dc4575
Philipp Schmitt <philipp@schmitt.co>
2023-02-10 10:26:48 +0100

    Funcs!

--- nmap-for-poor-people.sh
+++ nmap-for-poor-people.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+usage() {
+  echo "Usage: $0 [--timeout TIMEOUT] HOST PORT [PORT...]"
+}
+
 echo_success() {
   echo -e "✅ \e[32m${*}\e[0m"
 }
@@ -8,36 +12,51 @@ echo_error() {
   echo -e "🔴 \e[31m${*}\e[0m"
 }
 
-while [[ -n "$1" ]]
-do
-  case "$1" in
-    -t|--timeout)
-      TIMEOUT="$2"
-      shift 2
-      ;;
-    *)
-      break
-      ;;
-  esac
-done
+tcp_port_check() {
+  local h="$1" p="$2"
+  echo > "/dev/tcp/${h}/${p}" 2>/dev/null
+}
 
-HOST="$1"
-shift
-PORTS=("$@")
-
-if [[ -z "$HOST" ]] || [[ -z "${PORTS[*]}" ]]
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
-  echo "Usage: $0 [--timeout TIMEOUT] HOST PORT [PORT...]" >&2
-  exit 2
-fi
+  TIMEOUT=3
 
-for PORT in "${PORTS[@]}"
-do
-  MESSAGE="${HOST}:${PORT}"
-  if timeout "${TIMEOUT:-5}" echo > "/dev/tcp/${HOST}/${PORT}" 2> /dev/null
+  while [[ -n "$1" ]]
+  do
+    case "$1" in
+      -h|--help|-\?|help)
+        usage
+        exit 0
+        ;;
+      -t|--timeout)
+        TIMEOUT="$2"
+        shift 2
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+
+  HOST="$1"
+  shift
+  PORTS=("$@")
+
+  if [[ -z "$HOST" ]] || [[ -z "${PORTS[*]}" ]]
   then
-    echo_success "$MESSAGE open"
-  else
-    echo_error "$MESSAGE CLOSED"
+    usage >&2
+    exit 2
   fi
-done
+
+  for PORT in "${PORTS[@]}"
+  do
+    MESSAGE="${HOST}:${PORT}"
+
+    if timeout "$TIMEOUT" tcp_port_check "$HOST" "$PORT" &>/dev/null
+    then
+      echo_success "$MESSAGE open"
+    else
+      echo_error "$MESSAGE CLOSED"
+    fi
+  done
+fi
